2bd5f9ff124a0cc6369358bc5d79bb90c6620f60
zxjun <dolphin.jun@163.com>
2018-10-12 22:17:19 +0800

    验证运算

--- vs/intel_OpenCL_Convolution/CapsBasic/app.cpp
+++ vs/intel_OpenCL_Convolution/CapsBasic/app.cpp
@@ -1,4 +1,3 @@
-#include "stdafx.h"
 #include<stdio.h>
 #include<stdlib.h>
 #include<CL/cl.h>
@@ -34,15 +33,16 @@ int main(void)
 	/*=================================================
 	define parameters for image, filter, kernels
 	=================================================*/
-	int const W = 8;			//image width
-	int const H = 8;			//image height
+	int const W = 100;			//image width
+	int const H = 100;			//image height
 	int const K = 3;			//filter kernel size
 	int const Wn = (W + K - 1); //padded image width
 	int const Hn = (H + K - 1); //padded image height
 
 	int point_num = Wn * Hn;
 	int data_vecs[Wn*Hn];
-	int filter_coe[K*K] = { -1,0,1,-2,0,2,-1,0,1 }; //sobel filter: horizontal gradient
+	int filter_coex[K*K] = { -1,0,1,-2,0,2,-1,0,1 }; //sobel filter: horizontal gradient
+	int filter_coey[K*K] = { -1,0,1,-2,0,2,-1,0,1 }; //sobel filter: horizontal gradient
 	int i, j;
 
 	for (i = 0; i < point_num; i++)
@@ -54,9 +54,9 @@ int main(void)
 	printf("\n");
 	printf("Array data_in:\n");
 	for (i = 0; i < Hn; i++) {
-		printf("row[%d]:\t", i);
+		if(i<10)printf("row[%d]:\t", i);
 		for (j = 0; j < Wn; j++) {
-			printf("%d,\t", data_vecs[i*Wn + j]);
+			if(j<10)printf("%d,\t", data_vecs[i*Wn + j]);
 		}
 		printf("\n");
 	}
@@ -71,18 +71,62 @@ int main(void)
 	=================================================*/
 	kernel_str = (char *)malloc(MAX_SOURCE_SIZE);
 	result = (int *)malloc(W*H * sizeof(int));
+	//遍历系统中所有OpenCL平台
+	ret = clGetPlatformIDs(0, 0, &ret_num_platforms);
+	printf("Num_of_platforms: %d\n", ret_num_platforms);
+	cl_platform_id* platforms = new cl_platform_id[ret_num_platforms];
+	ret = clGetPlatformIDs(ret_num_platforms, platforms, 0);
+	cl_uint selected_platform_index = ret_num_platforms;
+	for (cl_uint i = 0; i < ret_num_platforms; ++i)
+	{
+		size_t platform_name_length = 0;
+		size_t maxComputeUnits = 0;
+		ret = clGetPlatformInfo(platforms[i],CL_PLATFORM_NAME,0,0,&platform_name_length);
+		// 调用两次，第一次是得到名称的长度
+		char* platform_name = new char[platform_name_length];
+		ret = clGetPlatformInfo(platforms[i],CL_PLATFORM_NAME,platform_name_length,platform_name,0);
+		printf("%d:%s", i, platform_name);
+		ret = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_GPU, 1, &device_id, NULL);
+		ret = clGetDeviceInfo(device_id, CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(cl_uint), &maxComputeUnits, NULL);
+		printf(" [PE=%d]", maxComputeUnits);
+		if (strstr(platform_name, "Intel") && selected_platform_index == ret_num_platforms)
+		{
+			printf(" [Selected]");
+			selected_platform_index = i;
+		}
+		printf("\n");
+		delete[] platform_name;
+	}
+	if (selected_platform_index == ret_num_platforms)
+	{
+		printf("Intel platforms NOT FOUND!!\n");
+		return 1;
+	}
 
-	ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
-	ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id,
-		&ret_num_devices);
+	cl_platform_id platform = platforms[selected_platform_index];
+	ret = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device_id, NULL);
 	context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
 	command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
-	fp = fopen("Conv2D.cl", "r");
+	fp = fopen("kernelfun.cl", "r");
 	kernel_code_size = fread(kernel_str, 1, MAX_SOURCE_SIZE, fp);
 	fclose(fp);
-	program = clCreateProgramWithSource(context, 1, (const char **)&kernel_str,
-		(const size_t *)&kernel_code_size, &ret);
+	program = clCreateProgramWithSource(context, 1, (const char **)&kernel_str, (const size_t *)&kernel_code_size, &ret);
 	ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
+#if 0
+	//Shows the log
+	char* build_log;
+	size_t log_size;
+	//First call to know the proper size
+	clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
+	build_log = new char[log_size + 1];
+	// Second call to get the log
+	clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, log_size, build_log, NULL);
+	build_log[log_size] = '\0';
+	printf("----------------\n");
+	printf("build_log:%s", build_log);
+	printf("----------------\n");
+	delete[] build_log;
+#endif
 	kernel = clCreateKernel(program, "Conv2D", &ret);
 	data_in = clCreateBuffer(context, CL_MEM_READ_WRITE, Wn*Hn * sizeof(int), NULL, &ret);
 	data_out = clCreateBuffer(context, CL_MEM_READ_WRITE, W*H * sizeof(int), NULL, &ret);
@@ -90,9 +134,8 @@ int main(void)
 
 	//write image data into data_in buffer
 	ret = clEnqueueWriteBuffer(command_queue, data_in, CL_TRUE, 0, Wn*Hn * sizeof(int), data_vecs, 0, NULL, NULL);
-
 	//write filter data into filter_in buffer
-	ret = clEnqueueWriteBuffer(command_queue, filter_in, CL_TRUE, 0, K*K * sizeof(int), filter_coe, 0, NULL, NULL);
+	ret = clEnqueueWriteBuffer(command_queue, filter_in, CL_TRUE, 0, K*K * sizeof(int), filter_coex, 0, NULL, NULL);
 
 	//set kernel arguments
 	ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&data_in);
@@ -105,23 +148,30 @@ int main(void)
 	global_item_size[1] = { H };
 	local_item_size[0] = { 1 };
 	local_item_size[1] = { 1 };
-
+	cl_event ev;
 	//execute data parallel kernel */
 	ret = clEnqueueNDRangeKernel(command_queue, kernel, work_dim, NULL,
-		global_item_size, local_item_size, 0, NULL, NULL);
-
-
+			global_item_size, local_item_size, 0, NULL, &ev);
+	clFinish(command_queue);
+	//计算kerenl执行时间 
+	cl_ulong startTime, endTime;
+	clGetEventProfilingInfo(ev, CL_PROFILING_COMMAND_START,
+		sizeof(cl_ulong), &startTime, NULL);
+	clGetEventProfilingInfo(ev, CL_PROFILING_COMMAND_END,
+		sizeof(cl_ulong), &endTime, NULL);
+	cl_ulong kernelExecTimeNs = endTime - startTime;
+	printf("GPU_1 running:%8.6f ms\n", kernelExecTimeNs*1e-6);
 	// read data_out to host
 	ret = clEnqueueReadBuffer(command_queue, data_out, CL_TRUE, 0,
-		W*H * sizeof(int), result, 0, NULL, NULL);
+			W*H * sizeof(int), result, 0, NULL, NULL);
 
 	//display output data
 	FILE *f_img_out = fopen("image_out.txt", "w+");
 	printf("Array data_out: \n");
 	for (i = 0; i < H; i++) {
-		printf("row[%d]:\t", i);
+		if (i<10)printf("row[%d]:\t", i);
 		for (j = 0; j < W; j++) {
-			printf("%d,\t", result[i*W + j]);
+			if(j<10)printf("%d,\t", result[i*W + j]);
 			fprintf(f_img_out, "%d,\t", result[i*W + j]);
 		}
 		printf("\n");
